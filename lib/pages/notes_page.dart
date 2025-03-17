f962aa9d2d98fca4ece0653e9b803b5800f29533
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-03-17 09:17:03 +0530

    note category

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes_sphere_v116/models/note_model.dart';
+import 'package:notes_sphere_v116/services/note_services.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
@@ -8,6 +10,40 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+  final NoteService noteService = NoteService();
+  List<NoteModel> allNotes = [];
+  Map<String, List<NoteModel>> notesWithCategory = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAndCreateData();
+  }
+
+  //check weather the user is new
+  void _checkAndCreateData() async {
+    final bool isNewUser = await noteService.isNewUser();
+    debugPrint("$isNewUser");
+    // if the user is new create the inital notes
+    if (isNewUser) {
+      await noteService.createInitialNotes();
+    }
+    _loadNotes();
+  }
+
+  //load the notes
+  Future<void> _loadNotes() async {
+    final List<NoteModel> loadedNotes = await noteService.loadNotes();
+    final Map<String, List<NoteModel>> notesByCategory =
+        noteService.getNotesByCategoryMap(loadedNotes);
+    setState(() {
+      allNotes = loadedNotes;
+      notesWithCategory = notesByCategory;
+      debugPrint("$notesWithCategory");
+      debugPrint("${allNotes.length}");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
